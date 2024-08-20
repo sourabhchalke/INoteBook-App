133207999fde8080fa5afcd5970a68031ba6c703
sourabhchalke <sourabhchalke2157@gmail.com>
2024-08-20 18:00:19 +0530

    storing data in mongo

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ connectToMongo();
 const app = express()
 const port = 3000
 
+//middleware
+app.use(express.json());
+
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
